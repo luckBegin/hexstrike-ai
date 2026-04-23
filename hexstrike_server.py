ef49fc65cd1ccd5d9617021c872139530bfd4345
Sirius.Black <ofghrt@qq.com>
2026-04-23 15:04:51 +0800

    ip

--- hexstrike_server.py
+++ hexstrike_server.py
@@ -17286,4 +17286,4 @@ if __name__ == "__main__":
         if line.strip():
             logger.info(line)
 
-    app.run(host="127.0.0.1", port=API_PORT, debug=DEBUG_MODE)
+    app.run(host="0.0.0.0", port=API_PORT, debug=DEBUG_MODE)
